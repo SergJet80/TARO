43d5ed9f1714802fc8fe14c902af5eedafe2c61b
SergJet80 <SergJet80@users.noreply.github.com>
2026-09-08 15:14:29 +0000

    CSS v3.5: подхват стилей footer-contact

--- webapp/cards_build.py
+++ webapp/cards_build.py
@@ -581,8 +581,8 @@ def document(title: str, description: str, url: str, body: str, article: bool, o
 <meta property="og:type" content="{'article' if article else 'website'}">
 <meta property="og:url" content="{escape(url, quote=True)}">
 {f'<meta property="og:image" content="{SITE}/img/cards/{escape(og_img, quote=True)}">' if og_img else ''}
-<link rel="stylesheet" href="/css/style.css?v=3.4">
-<link rel="stylesheet" href="/css/astrology.css?v=3.4">
+<link rel="stylesheet" href="/css/style.css?v=3.5">
+<link rel="stylesheet" href="/css/astrology.css?v=3.5">
 <style>{STYLE}</style>
 <script type="application/ld+json">
 {schema}
